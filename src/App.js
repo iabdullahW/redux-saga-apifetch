14d1d09944c46f6c8b62f006965b673d9086a416
abdullah <as0950507@gmail.com>
2024-11-06 19:40:08 +0500

    F

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsersFetch } from "./actions";
+// import FeatureComponent from "./components/FeatureComponent";
 
 function App() {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.myFirstReducer.users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-zinc-800 items-center justify-center flex flex-col gap-4">
+    <h1>Redux Saga Basic Api fetching</h1>
+      <button
+        onClick={() => dispatch(getUsersFetch())}
+        className="bg-white border-zinc-600 ring-2 ring-blue-500  p-2 px-4 rounded-md"
+      >
+        click
+      </button>
+      <div className="flex flex-col gap-2">
+        <span className="text-white font-bold text-2xl bg-purple-300 rounded-md ">Users:</span>
+        {users.map((user) => (
+          <li className=" list-none bg-rose-400 rounded-md"><span className=""> {user.name} -- {user.email}</span></li>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { combineReducers, createStore , applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import myFirstReducer from "./reducer";
+import mySaga from "./sagas";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const rootReducer = combineReducers({ myFirstReducer });
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(mySaga);
+
+
+// Create root and render app using React 18's createRoot API
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance if needed
 reportWebVitals();
